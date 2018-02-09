cb7687e0fa210f807f62bee2808a421467e7e797
Michael Lancaster <michaell.llancaster@gmail.com>
2018-02-08 23:50:25 -0800

    Initial normalizer algorithm
    Implement initial algorithm to traverse the bookmarks tree and normalize/flatten for solshal

--- app.py
+++ app.py
@@ -15,18 +15,70 @@ def chrome_importer():
         app
     
     Returns:
-        [Dict] -- Formatted collection for Solshal
+        [List] -- List of folders and it's collections
     """
-
-    data = request.get_json()["data"]
-    formattedData = format_data(data)
+    bookmarks = request.get_json()["bookmarks"]
+    formattedData = get_collections(bookmarks)
 
     return jsonify({
         "data": formattedData
-    })
+    }), 200
 
-def format_data(data):
-    return data
+def filter_folders(folders):
+    """Filter folders that only contain collections
+    
+    Arguments:
+        folders {[Dict]} -- Folders containing collections
+    
+    Returns:
+        [List] -- List of folders and it's collections
+    """
+    # List containing folders and it's collections
+    filteredFolders = []
+    for key in folders:
+        current = folders[key]
+        if not current["collections"]:
+            continue
+        
+        filteredFolders.append({
+            "folder_name": current["title"],
+            "collections": current["collections"]
+        })
+    
+    return filteredFolders
+
+def get_collections(data):
+    """Traverse a tree of collection find folders and it's bookmarks
+    then flatten them out
+    
+    Arguments:
+        data {[List]} -- bookmark list tree
+    """
+    # Dict containing all folders and it's bookmarks collection
+    flattenedFolders = {}
+
+    def flatten_folders(bookmarks):
+        for collection in bookmarks:
+            if not "url" in collection:
+                folderId = collection["id"]
+                flattenedFolders[folderId] = {
+                    "title": collection["title"],
+                    "collections": []
+                }
+
+            if "url" in collection:
+                parentId = collection["parentId"]
+                flattenedFolders[parentId]["collections"].append({
+                    "url": collection["url"],
+                    "title": collection["title"]
+                })
+                continue
+            
+            if "children" in collection:
+                flatten_folders(collection["children"])
+    
+    flatten_folders(data)
+    return filter_folders(flattenedFolders)
 
 if __name__ == "__main__":
     HOST = "0.0.0.0"
